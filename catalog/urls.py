243d01e2fe4616778ca76c63f6231303e832b5cd
Daureo Silva <daureo@hp-zbook-15u-g3.hpm.local>
2020-11-09 16:43:18 -0300

    Detail Book page implemented

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
     path('books/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    #re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'), #Could be used the same results as the line above
 ]
